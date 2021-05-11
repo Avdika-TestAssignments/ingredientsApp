19459175a1bb5f8a3f2242b9ad0cc03ba22a5811
Avdika <avdika@ukr.net>
2021-05-11 10:51:37 +0300

    changed history window position

--- src/components/QueryHistory.js
+++ src/components/QueryHistory.js
@@ -5,7 +5,7 @@ import { Box } from "@chakra-ui/react"
 function QueryHistory({ onClick, queryHistory }) {
     console.log('__ queryHistory : ', queryHistory);
     return (
-        <Box maxW="50%"  className='historyWindow'>
+        <Box pos="absolute" left="25%" maxW="50%"  className='historyWindow'>
             <ul id="searchHistory">
             {queryHistory.map((historyElement, index) =>
                     index < 10 && <li key={uuid()} onClick={() => onClick(historyElement)}>{historyElement.query}</li>
